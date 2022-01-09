1294f87ed0f1e8d6da46eadc760256b526049a53
Sławomir Kurczewski <kurczewski7@gmail.com>
2022-01-09 23:19:41 +0100

    Initial Commit

---
+++ PrzenoszenieOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrzenoszenieOne
+//
+//  Created by Sławomir Kurczewski on 09/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
