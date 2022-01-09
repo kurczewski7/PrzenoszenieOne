797ad963470e03c2ec4c3f6b9b3589f38f0f4d6a
Sławomir Kurczewski <kurczewski7@gmail.com>
2022-01-10 00:01:24 +0100

    start

--- PrzenoszenieOne/ViewController.swift
+++ PrzenoszenieOne/ViewController.swift
@@ -6,12 +6,17 @@
 //
 
 import UIKit
+import SSZipArchive
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        zaczynaj()
+    }
+    func zaczynaj() {
+        SSZipArchive.unzipFile(atPath: "dane.zip", toDestination: "/DUPA")
     }
 
 
